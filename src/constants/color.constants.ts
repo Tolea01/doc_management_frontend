4ab4bfc4962b522e4f05658f23e3d2f1bf069a2c
Tolea01 <anatoliecurbat@gmail.com>
2024-12-03 21:16:28 +0200

    initial project config

---
+++ src/constants/color.constants.ts
@@ -0,0 +1,4 @@
+export const COLORS = {
+  primary: 'rgb(37, 99, 235)',
+  text_color: 'rgb(21, 23, 42)',
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,25 @@
-import type { Config } from "tailwindcss";
+import { COLORS } from './src/constants/color.constants';
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: 'class',
+  mode: 'jit',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      colors: COLORS,
+      screens: {
+        xs: '320px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
       },
     },
   },
