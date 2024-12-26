7c932dba9d03003bff74b85f55d8b041c0a871ca
Tolea01 <anatoliecurbat@gmail.com>
2024-12-26 10:50:37 +0200

    code review

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ro">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <Providers>
           {children}
           <Toaster
