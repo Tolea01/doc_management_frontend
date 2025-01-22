385ff213e120bdd9c5888de1218f0ad8216d34d6
Tolea01 <anatoliecurbat@gmail.com>
2025-01-22 20:51:09 +0200

    modal

--- src/app/management/director/dashboard/page.tsx
+++ src/app/management/director/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import { NO_INDEX_PAGE } from '@constants/seo.constants';
 import { Metadata } from 'next';
+import Modal from '@components/modal/Modal';
 
 export const metadada: Metadata = {
   ...NO_INDEX_PAGE,
 };
 
 export default function directorPage() {
-  return <p>director dashboard</p>;
+  return <Modal />;
 }
