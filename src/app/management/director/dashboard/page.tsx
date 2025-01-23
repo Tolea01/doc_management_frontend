0b223102f972f1d323e456ae3ea23c9cdee5337a
Tolea01 <anatoliecurbat@gmail.com>
2025-01-23 22:23:20 +0200

    modal component

--- src/app/management/director/dashboard/page.tsx
+++ src/app/management/director/dashboard/page.tsx
@@ -1,11 +1,10 @@
 import { NO_INDEX_PAGE } from '@constants/seo.constants';
 import { Metadata } from 'next';
-import Modal from '@components/modal/Modal';
 
 export const metadada: Metadata = {
   ...NO_INDEX_PAGE,
 };
 
 export default function directorPage() {
-  return <Modal />;
+  return <p>director page</p>;
 }
--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -1,58 +1,33 @@
 'use client';
 
-import { useState } from 'react';
+import './style.css';
+import Button from '@components/buttons/Button';
+import React from 'react';
 
-export default function Modal() {
-  const [isOpen, setIsOpen] = useState(false);
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => any;
+  modalHeader: string;
+  modalBody: React.ReactNode;
+}
 
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
-
+export default function Modal({
+  isOpen,
+  onClose,
+  onConfirm,
+  modalHeader,
+  modalBody,
+}: ModalProps) {
   return (
     <>
-      <div className="mb-3 flex gap-3">
-        <button
-          onClick={openModal}
-          className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-          type="button"
-        >
-          Open Modal XL
-        </button>
-        <div
-          className={`fixed inset-0 z-[1000] grid h-screen w-screen place-items-center bg-black bg-opacity-60 backdrop-blur-sm transition-opacity duration-500 ${
-            isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
-          }`}
-        >
-          <div
-            className={`relative m-4 p-4 w-3/4 rounded-lg bg-white shadow-sm transition-transform duration-500 ${
-              isOpen ? 'scale-100 opacity-100' : 'scale-90 opacity-0'
-            }`}
-          >
-            <div className="flex shrink-0 items-center pb-4 text-xl font-medium text-slate-800">
-              It's a simple modal.
-            </div>
-            <div className="relative border-t border-slate-200 py-4 leading-normal text-slate-600 font-light">
-              The key to more success is to have a lot of pillows. Put it this way, it
-              took me twenty-five years to get these plants, twenty-five years of blood,
-              sweat, and tears, and I&apos;m never giving up, I&apos;m just getting
-              started. I&apos;m up to something. Fan luv.
-            </div>
-            <div className="flex shrink-0 flex-wrap items-center pt-4 justify-end">
-              <button
-                onClick={closeModal}
-                className="rounded-md border border-transparent py-2 px-4 text-center text-sm transition-all text-slate-600 hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                type="button"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={closeModal}
-                className="rounded-md bg-green-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-                type="button"
-              >
-                Confirm
-              </button>
-            </div>
+      <div className={`modal-overlay ${isOpen ? 'modal-overlay--visible' : ''}`}>
+        <div className={`modal-container ${isOpen ? 'modal-container--visible' : ''}`}>
+          <div className="modal-header">{modalHeader}</div>
+          <div className="modal-body">{modalBody}</div>
+          <div className="modal-footer">
+            <Button value="închide" variant="secondary" size="medium" onClick={onClose} />
+            <Button value="confirm" variant="primary" size="medium" onClick={onConfirm} />
           </div>
         </div>
       </div>
